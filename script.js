e82c14305a527b31fb00c227242ef7fa9ac8dfc0
Orazulike Chinaza <orazulikechinaza@gmail.com>
2024-12-31 08:56:44 +0000

    submit button responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.querySelector("form").addEventListener("submit", function (e) {
     e.preventDefault();
-    alert("Thank you for reaching! I'll get back to you soon.");
+    alert("Thank you for reaching out ! I'll get back to you soon.");
   });
    
