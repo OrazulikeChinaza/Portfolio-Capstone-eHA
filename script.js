8c7dd1989e9c365575480416eeed098891538e51
Orazulike Chinaza <orazulikechinaza@gmail.com>
2025-01-03 13:05:35 +0000

    form functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelector("form").addEventListener("submit", function (e) {
     e.preventDefault();
+    this.reset();
     alert("Thank you for reaching out ! I'll get back to you soon.");
   });
    
