949a85a898d1f3877dbb545edbc388cecfcac38f
Orazulike Chinaza <orazulikechinaza@gmail.com>
2025-01-03 18:28:23 +0000

    resume pdf download

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ document.querySelector("form").addEventListener("submit", function (e) {
     alert("Thank you for reaching out ! I'll get back to you soon.");
   });
    
+  function downloadResume() {
+    const link = document.createElement('a');
+    link.href = "img/chinaza's resume.pdf";
+    link.download = "Chinaza-Resume.pdf";
+    link.click();
+  }
